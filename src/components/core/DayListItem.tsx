0a8ef883d774b7ccae594ec63247bff8d2d99e5c
= <=>
2024-01-30 13:53:56 +0530

    DayItemList Added

---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,32 @@
+import { Text, View, StyleSheet } from "react-native";
+
+interface DayListItemProps {
+  day: number;
+}
+
+function DayListItem({ day }: DayListItemProps) {
+  return (
+    <View style={styles.box}>
+      <Text style={styles.text}>{day}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#F9EDE3",
+    flex: 1,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 20,
+    borderColor: "#9b4521",
+  },
+  text: {
+    color: "#9B4521",
+    fontSize: 70,
+  },
+});
+
+export default DayListItem;
